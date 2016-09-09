cd74840dec07d626475d60c350adea145d7f0d09
SatarovArthur <arthursatarov@gmail.com>
2016-09-09 14:47:05 +0500

    Обновил код

--- index.php
+++ index.php
@@ -26,6 +26,7 @@
     <link rel="apple-touch-icon" href="apple-touch-favicon.png"/>
 
 
+   
     <script>
      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
