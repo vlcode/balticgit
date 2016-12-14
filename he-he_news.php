0e2bc2edeba8c06faabe80f91ad7a905a26991a4
SatarovArthur <arthursatarov@gmail.com>
2016-12-14 19:21:15 +0500

    Новая новость

---
+++ he-he_news.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="description" content="У вас есть возможность получить грант на обучение за рубежом от Балтийского Центра. Стань студентом Даугавпилсского Университета и мы оплатим первый год обучения">
+    <!--<meta name="keywords" content="Описание">-->
+    <meta name="google-site-verification" content="S386zaI2q9nFsR9pmcPC1nt3toyeSqg1xFdgyrWleA8" />
+    <meta name="google-site-verification" content="xHMOb3cAs40Fa2r6LM5jKQyKLoZRRVaXkiSIKVDUoBk" />
+    <title>Health&amp;Help - меняя мир к лучшему.</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/hover.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="favicon.png" />
+    <link rel="apple-touch-icon" href="apple-touch-favicon.png" />
+    <script src="js/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-76055340-1']);
+        _gaq.push(['_trackPageview']);
+        (function () {
+            var ga = document.createElement('script');
+            ga.type = 'text/javascript';
+            ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(ga, s);
+        })();
+    </script>
+</head>
+
+<body>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function () {
+                try {
+                    w.yaCounter36592180 = new Ya.Metrika({
+                        id: 36592180
+                        , clickmap: true
+                        , trackLinks: true
+                        , accurateTrackBounce: true
+                        , webvisor: true
+                    });
+                }
+                catch (e) {}
+            });
+            var n = d.getElementsByTagName("script")[0]
+                , s = d.createElement("script")
+                , f = function () {
+                    n.parentNode.insertBefore(s, n);
+                };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            }
+            else {
+                f();
+            }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/36592180" style="position:absolute; left:-9999px;" alt="" /></div>
+    </noscript>
+    <!-- /Yandex.Metrika counter -->
+    <?php require 'blocks/header.php'; ?>
+        <div class="contaner-fluid additional-color">
+            <!--  Breadcrumbs-->
+            <div class="container hlebnie-kroshki">
+                <div class="row">
+                    <ol class="breadcrumb">
+                        <li><a href="index.php">Главная</a></li>
+                        <li><a href="news.php">Новости</a></li>
+                        <li class="active">Текущее объявление</li>
+                    </ol>
+                </div>
+            </div>
+            <!--About    -->
+            <section>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1>Health&amp;Help - меняя мир к лучшему.</h1> <img src="img/news/he-he_2_news.jpg" alt="Грант на обучение" class="img-responsive">
+                            <p>Всегда удивительно встречать людей, отличающихся от других. Людей для которых новые впечатления и возможность помогать другим людям стоят гораздо дороже любых денег мира.</p>
+                            <p>И да, мы встретили таких людей - каждый день меняющих мир к лучшему. Рады сообщить что Балтийский Центр сотрудничает с благотворительной организацией Health&amp;Help, в рамках которой более сотни человек объединились для того, чтобы дать гватемальским индейцам Майя доступ к базовой медицинской помощи.</p>
+                            <p>В январе 2016 года в рамках проекта была запущена первая краудфандинговая кампания, собравшая 1 миллион 327 тысяч рублей на первый этап строительства клиники в Гватемале. Строительство клинику успешно подходит к концу силами таких же молодых людей как и ВЫ!</p>
+                            <p>В команде проекта множество как дистанционных, так и реальных волонтеров приезжающих в Гватемалу познавать этот мир, помогая тем, кому необходима помощь.</p>
+                            <p><b>Что это значит для наших студентов?</b></p>
+                            <p>Самое главное, вы можете присоединиться к команде молодых, целеустремленных людей. Быть частью команды для которой нет ничего невозможного, и возможно у вас есть, чем удивить остальных.</p>
+                            <p>Увидеть новую, прекрасную в своей самобытности страну и применить свои профессиональные навыки в реальном деле. Хотите стать лучшим? Дерзайте!
+                                <br> Это действительно очень интересно, и знаете что? Вам будет что вспомнить в будущем!</p>
+                            <p><b>Балтийский Центр и Health&amp;Help предлагают:</b></p>
+                            <ol>
+                                <li>Стажировка в Гватемале - для студентов последних курсов медицинского университета.</li>
+                                <li>Стажировка в Гватемале - для студентов педагогических университетов.</li>
+                                <li>Стажировка в Гватемале - для студентов строительных университетов и колледжей.</li>
+                                <li>Стажировка в Гватемале - для студентов последних курсов лингвистических университетов, факультеты испанского и английских языков.</li>
+                                <li>А так же энтузиасты фотографы, видеооператоры, помощники!</li>
+                            </ol>
+                        </div>
+                    </div>
+            </section>
+            </div>
+            <?php require 'blocks/footer.php'; ?>
+                <script src="js/bootstrap.min.js"></script>
+                <!--BEGIN JIVOSITE CODE {literal} -->
+                <script type='text/javascript'>
+                    (function () {
+                        var widget_id = '9B8okHwNjb';
+                        var d = document;
+                        var w = window;
+
+                        function l() {
+                            var s = document.createElement('script');
+                            s.type = 'text/javascript';
+                            s.async = true;
+                            s.src = '//code.jivosite.com/script/widget/' + widget_id;
+                            var ss = document.getElementsByTagName('script')[0];
+                            ss.parentNode.insertBefore(s, ss);
+                        }
+                        if (d.readyState == 'complete') {
+                            l();
+                        }
+                        else {
+                            if (w.attachEvent) {
+                                w.attachEvent('onload', l);
+                            }
+                            else {
+                                w.addEventListener('load', l, false);
+                            }
+                        }
+                    })();
+                </script>
+                <!-- {/literal} END JIVOSITE CODE -->
+</body>
+
+</html>
